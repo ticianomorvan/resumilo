6a3fe11e9742c619aec15bc58c41888180ba088f
Ti7oyan <ticiano.morvan@protonmail.com>
2022-09-25 09:42:19 -0300

    remove prisma as the server with the database will be replaced for firebase

--- pages/api/get.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient, resumen } from "@prisma/client";
-import { NextApiRequest, NextApiResponse } from "next";
-
-const prisma = new PrismaClient()
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse<resumen[]>) {
-  const resumenes = await prisma.resumen.findMany()
-  res.json(resumenes)
-}
