900592aa65ff23640f9dad0b44db5f3decc5945d
Ti7oyan <ticiano.morvan@protonmail.com>
2022-10-02 19:32:26 -0300

    remove old code and put temporal placeholders

--- pages/resumenes/index.tsx
+++ pages/resumenes/index.tsx
@@ -1,17 +1,3 @@
-import dynamic from "next/dynamic";
-import { Suspense } from "react";
-import BaseLayout from "../../components/layouts/BaseLayout";
-const Search = dynamic(() => import("../../components/search"));
-
-const Index = () => {
-  return (
-    <BaseLayout title="Resúmenes">
-      <h1 className="text-4xl lg:text-6xl">Comenzá a buscar</h1>
-      <Suspense fallback={`Loading...`}>
-        <Search />
-      </Suspense>
-    </BaseLayout>
-  );
-};
-
-export default Index;
+export default function Index({}) {
+  return <p>Index...</p>;
+}
